8326188fe066debd98a98f9999ec4ab0e95ce506
kelvinjoshua <kelvinbale78@gmail.com>
2020-04-02 18:22:41 +0300

    declared attributes outside the constructor

--- src/app/properties.ts
+++ src/app/properties.ts
@@ -1,13 +1,13 @@
 export class Properties {
    
     showauthor:Boolean;//to be used explicitely elesewhere for toggling between hiding and showing an author//
- 
+    public vote:number;
+    public downvote:number;
     constructor(
         public message:string,
         public author:string,
         public completeDate:Date,
-        public vote:number,
-        public downvote:number,
+
     ){
         this.showauthor=false;//initialised false so that the author isnt displayed//
         //after this initialisation proceed to the properties array and ake changes(new)//
